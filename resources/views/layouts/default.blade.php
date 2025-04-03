182930291d95322faa5e5d749de1075de7c7bd18
dominic <zzp208@gmail.com>
2025-04-03 11:47:58 +0800

    登陆成功后的导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,7 +12,8 @@
         @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
-  </div>
+    </div>
 </div>
+<script src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
