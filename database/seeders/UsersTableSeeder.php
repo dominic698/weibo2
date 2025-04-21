b474e1d35ea1659f7cd92f6075b0c8152c5a24ae
dominic <zzp208@gmail.com>
2025-04-21 20:08:49 +0800

    管理员可删用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'dominic';
         $user->email = 'zzp208@gmail.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
