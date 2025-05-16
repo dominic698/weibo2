3f65e638db8f05e552914d7f5b9fda674ddcd4b7
dominic <zzp208@gmail.com>
2025-05-16 15:13:58 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,4 +67,35 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderByDesc('created_at');
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_id)
+    {
+        if (!is_array($user_id)) {
+            $user_id = compact('user_id');
+        }
+        $this->followings()->sync($user_id, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
